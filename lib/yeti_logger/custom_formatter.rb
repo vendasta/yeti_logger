02ee68c20b6870f421a0c1025203c42f83e925f7
Antoniu Vadan <avadan@vendasta.com>
2024-03-01 16:50:18 -0600

    use UTC timestamp, add comments to formatter class, refactor logic according to comments

--- lib/yeti_logger/custom_formatter.rb
+++ lib/yeti_logger/custom_formatter.rb
@@ -1,5 +1,8 @@
+# CustomFormatter is a custom Rails log formatter.
+# It has support for adding arbitrary tags to every log created by the Rails logger.
+# Tag values are generated at log creation time.
 class YetiLogger::CustomFormatter
-  # @param tags [Hash] - maps names of tags to procs that return their value
+  # @param tags [Hash] - maps names of tags to procs that return their value.
   def initialize(tags = {})
     super()
     @tags = tags
@@ -13,21 +16,19 @@ class YetiLogger::CustomFormatter
   # @param severity [String]
   # @param time [Time] unused
   # @param progname [String] unused
-  # @param msg [String]
+  # @param msg [String] - log body
   def call(severity, time, progname, msg)
-    timestamp = "#{Time.now.iso8601(3)}"
+    timestamp = Time.now.utc.iso8601(3)
     pid = Process.pid
     msg = msg.inspect unless msg.is_a?(String)
     msg = "#{msg}\n" unless msg[-1] == ?\n
-    log_str = "#{timestamp} pid=#{pid} "
-    @tags.to_h.each do |k, v|
-      tag_val = v.call
-      if !tag_val.blank?
-        log_str += "#{k}=#{tag_val} "
-      end
-    end
+    log_str = "#{timestamp} pid=#{pid}"
 
-    log_str + "[#{severity}] - #{msg}"
+    tag_str = @tags.map { |k, v|
+      value = v.call
+      "#{k}=#{value}" unless value.to_s.empty?
+    }.compact.join(' ')
+
+    "#{log_str}#{tag_str.empty? ? '' : ' ' + tag_str} [#{severity}] - #{msg}"
   end
 end
-
